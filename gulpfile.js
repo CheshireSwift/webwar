8ca9cd5ca42b81136ca78fa706aa99a52e1ed511
Sam Collard <swift@swifteagle.co.uk>
2016-11-17 16:36:29 +0000

    Use Handlebars for views

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,27 @@
 var gulp = require('gulp')
 var ts = require('gulp-typescript')
 
-gulp.task('browser', function() {
+gulp.task('compileClient', function() {
   var tsProject = ts.createProject('tsconfig-browser.json')
   return tsProject.src()
     .pipe(tsProject())
     .js.pipe(gulp.dest('dist'))
 })
 
-gulp.task('server', ['browser'], function() {
+gulp.task('browser', ['compileClient'])
+
+gulp.task('compileServer', function() {
   var tsProject = ts.createProject('tsconfig-server.json')
   return tsProject.src()
     .pipe(tsProject())
     .js.pipe(gulp.dest('dist'))
 })
+
+gulp.task('copyServer', function() {
+  return gulp.src(['src/server/**/*', '!**/*.ts'])
+    .pipe(gulp.dest('dist/server'))
+})
+
+gulp.task('server', ['compileServer', 'copyServer'])
 
 gulp.task('default', ['browser', 'server'])
--- src/server/main.ts
+++ src/server/main.ts
@@ -1,10 +1,16 @@
+import path = require('path')
 import * as express from 'express'
 
 var app = express()
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 
 app.get('/', function (req: any, res: any) {
-  res.send('Hello World!')
+  res.render('index', { words: 'Hello, world!' })
 })
+
+const publicPath = path.join(__dirname, '../public')
+app.use('/public', express.static(publicPath))
 
 const port = process.env['PORT'] || 9000
 app.listen(port, function () {
