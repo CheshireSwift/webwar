6e09898dc31d59f2d13e8f6b44c5482b6cb4185a
Sam Collard <swift@swifteagle.co.uk>
2016-11-17 13:33:11 +0000

    Express Hello World

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,5 +1,12 @@
-function hello(compiler: string) {
-  console.log(`hello from ${compiler}`)
-}
+import * as express from 'express'
 
-hello('typescript')
+var app = express()
+
+app.get('/', function (req: any, res: any) {
+  res.send('Hello World!')
+})
+
+const port = process.env['PORT'] || 9000
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`)
+})
