9e2659c91d284aac41b2ebc932ada84096af970e
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2016-11-21 19:59:59 +0000

    Included partials for hbs

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,9 +1,11 @@
-import path = require('path')
+import path = require('path');
+import * as hbs from 'hbs';
 import * as express from 'express'
 
 var app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', function (req: any, res: any) {
   res.render('infantry', { scripts: ['main'] })
