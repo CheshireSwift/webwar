e4b90ca8bc07d0d7cc1f7b78cec91022655fba20
Todd <todd@softwire.com>
2016-11-21 22:02:56 +0000

    WIP: load map from store

--- src/public/mapGrid.ts
+++ src/public/mapGrid.ts
@@ -1,23 +1,41 @@
-import fs = require('fs')
+import path = require('path')
+import $ = require('jquery');
 import * as Buffer from 'buffer'
 
 window.onload = function() {
-  alert("did both");
   document.getElementById('loadMap').onclick = function() { loadMap() }
 }
 
 function loadMap(): void {
-  var file = 'store/testMap.json'
-  var filepath = __dirname + '/' + file
+  var file = 'testMap.json'
+  var filepath = path.join(__dirname, file)
   try {
-    var jsonFile = readJsonFileSync(filepath);
+
+
+    $.getJSON( filepath, function( data ) {
+      var items: string[] = [];
+      $.each( data, function( key, val ) {
+        items.push( "<li id='" + key + "'>" + val + "</li>" );
+      });
+
+      $( "<ul/>", {
+        "class": "my-new-list",
+        html: items.join( "" )
+      }).appendTo( "body" );
+    });
+
+
+    /*$.ajax({url: filepath, success: function(result){
+        var parsedJSON = JSON.parse(result)
+        $("#div1").html(parsedJSON);
+    }});*/
   } catch(exception) {
+    console.log($);
   }
-  //console.log("wtf m9 how can this line cause an error");
   return
 }
 
 function readJsonFileSync(filepath: string, encoding: Buffer.Encoding = 'utf8'): any{
-  var file: string = fs.readFileSync(filepath, encoding)
-  return JSON.parse(file)
+  //var file: string = fs.readFileSync(filepath, encoding)
+  //return JSON.parse(file)
 }
